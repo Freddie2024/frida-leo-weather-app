14ac0e9d7a3eef558b443b3e63f1ebaec60b4ad8
leonel-acosta <leoacosta88@gmail.com>
2024-10-10 10:02:19 +0200

    add styling to filter title

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,15 +4,15 @@ import ListItem from "../ListItem/ListItem";
 
 export default function List({ activities = [], isGoodWeather }) {   
     return (
-        <section>
+        <section className="activities__list">
         <h2>{isGoodWeather ? "Good Weather Activities" : "Bad Weather Activities"}</h2>
         <ul className="list">
             {activities.map(({ id, name, category, isForGoodWeather }) => (
                 <ListItem 
-                key={id}
-                name={name}
-                category={category} 
-                isForGoodWeather={isForGoodWeather}
+                    key={id}
+                    name={name}
+                    category={category} 
+                    isForGoodWeather={isForGoodWeather}
                 />
                ))}
            </ul>
--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -2,9 +2,9 @@ import "./ListItem.css"
 
 export default function ListItem({name, category, isForGoodWeather}) {
     return (
-    <li className={`${ isForGoodWeather === true ? "goodweather" : "badweather" }`}>
-        <h3>{name}</h3>
-        <p className="category">Category: {category}</p>
-        {isForGoodWeather === true ? <p className="weather">For good weather</p> : ""}
-    </li>)
+        <li className={`${ isForGoodWeather === true ? "goodweather" : "badweather" }`}>
+            <h3>{name}</h3>
+            <p className="category">Category: {category}</p>
+            {isForGoodWeather === true ? <p className="weather">For good weather</p> : ""}
+        </li>)
 }
